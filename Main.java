48d5377c10c4f3abe5841dec7529e433b4a42f0d
NihalNawaz <100338421+NihalNawaz@users.noreply.github.com>
2022-02-26 17:19:44 +0400

    Update Main.java
    Added three more controllers (part 2 )

--- Main.java
+++ Main.java
@@ -12,7 +12,9 @@ public class Main {
      
         Model model = new Model();               // Create shared database
         Controller1 c1 = new Controller1(model); // Create first controller
-        Controller2 c2 = new Controller2(model); // Create second controller
+        Controller2 c2 = new Controller2(model, "Controller2"); // Create second controller
+        Controller2 c3 = new Controller2(model, "Controller3")// Create thrid controller 
+        Controller2 c4 = new Controller2(model, "Controller4") // Create forth controller 
         
     } // main
   
